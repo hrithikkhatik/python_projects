1a0794593e93e8fd16380cf11828ce8756fa6d50
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-10-16 11:01:40 +0530

    Add files via upload

---
+++ Personal Info Greeting Program.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+favorite_color = input("Enter your favorite color: ")
+
+print(f"Hello {name}, thank you for using our program!")
+print(f"You are {age} years old, and your favorite color is {favorite_color}.")
+print("Thank you for your input, have a great day!")
